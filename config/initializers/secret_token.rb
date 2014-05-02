1f4b390ae216b9fd7f13e7545316dc0658d74327
Jacob Rosenberg <jake@lendup.com>
2014-05-02 13:33:14 -0700

    adds configs

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -23,4 +23,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Squash::Application.config.secret_key_base = '_SECRET_'
+Squash::Application.config.secret_key_base = '28db5e755aa8d609ff16a0b942f15ac9'
